536c2f7501a461e0031795f7a267e391e3507f05
Adrian POIGET <adrian.poiget@viveris.fr>
2020-05-13 20:57:52 +0200

    Better Tk UI and error management

--- dice.py
+++ dice.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import random, argparse, tkinter as tk
-
 
 class Dice():
 
@@ -12,45 +25,78 @@ class Dice():
 
     def roll(self, throws = 1):
         self._result = []
-        for t in range(0, throws):
-            self._result.append(random.randint(0 if self._include_zero else 1, self._sides))
+        try:
+            for t in range(0, throws):
+                self._result.append(random.randint(0 if self._include_zero else 1, self._sides))
+        except :
+            raise Exception("Error to perform dice rolling")
         return tuple(self._result)
 
     def check_sides(self, sides):
         return sides if sides >= 2 else None
 
-
-# Interface : nb de face, include zero, nombre de lancé
 class DiceGUI():
     def __init__(self):
         self.root = tk.Tk() #main window and other widgets
         self.root.title('Dices')
-        self.root.resizable(False, False)
+        self.root.minsize(360, 240)
+        self.root.resizable(True, True)
 
         # Input
-        self.label_minutes = tk.Label(text='Time for Backup (Minutes): ', height=2)
-        self.spinbox_minutes = tk.Spinbox(from_=1, to=100, width=5)
-        self.label_minutes.grid(row=0, column=1, sticky=tk.W)
+        self.frame_input = tk.Frame(self.root, height = 5)
+
+        ## Sides input
+        self.label_sides = tk.Label(self.frame_input, text='Number of sides: ', height=2, width=16 )
+        self.spinbox_sides = tk.Spinbox(self.frame_input, from_=2, to=1000, width=5, textvariable=tk.StringVar(value="6"))
+        self.label_sides.grid(row=0, column=0, sticky=tk.E+tk.W )
+        self.spinbox_sides.grid(row=0, column=1, sticky=tk.W)
+
+        ## Throw input
+        self.label_throws = tk.Label(self.frame_input,
+                                     text='Number of throws: ',
+                                     height=2)
+        self.spinbox_throws = tk.Spinbox(self.frame_input,
+                                         from_=1,
+                                         to=100,
+                                         width=5)
+        self.label_throws.grid(row=2, column=0, sticky=tk.E+tk.W)
+        self.spinbox_throws.grid(row=2, column=1, sticky=tk.W)
+
+        self.frame_input.pack(side="top", fill="y", expand=False)
 
         # Roll button command
-        self.button = tk.Button(cursor='hand2', activebackground='green',
-            font='bold 16',text="Roll", command=self.roll)
-        self.button.grid(row=1, padx=20, pady=10)
+        self.button = tk.Button(cursor='hand2',
+            activebackground='green',
+            font='bold 16',text="Roll", width=10, height=2,
+            command=self.roll)
+        self.button.pack(fill="x", expand=False, padx=10)
 
         # Dice output
-        self.label_res = tk.StringVar(value="Results")
-        self.label = tk.Label( textvariable=self.label_res)
-        self.label.grid(row=1, sticky=tk.E)
+        self.string_result = tk.StringVar(value="Results")
+        self.label_result = tk.Label(textvariable=self.string_result, font='bold 12')
+        self.label_result.pack(fill="both", expand=False)
 
-        self.label_dice_output = tk.StringVar()
-        self.label = tk.Label( textvariable=self.label_dice_output)
-        self.label.grid(row=1, column=1, sticky=tk.W)
+        self.frame_output = tk.Frame(self.root)
+        self.string_dice_output = tk.StringVar()
+        self.label_dice_output = tk.Message(self.frame_output, justify="left",textvariable=self.string_dice_output, font='8')
+        self.label_dice_output.grid(row=1, sticky=tk.E+tk.W)
+
+        self.frame_output.pack(side="bottom", fill="none",  expand=True)
 
         self.root.mainloop()
 
     def roll(self):
-        d = Dice()
-        self.label_dice_output.set(d.roll())
+        resuls = None
+        try:
+            d = Dice(sides=int(self.spinbox_sides.get()))
+            resuls = d.roll(int(self.spinbox_throws.get()))
+        except :
+            print("Error to perform dice rolling")
+            tk.messagebox.showerror("Error", "Error to perform dice rolling\nCheck sides and throw.")
+            return
+
+        digit_len = len(str(abs(max(resuls))))
+        self.string_dice_output.set(" - ".join([ '{nb:0>{max_len}}'.format(nb=dd, max_len=digit_len) for dd in resuls ]))
 
 def diceInteractive():
     raw_side = input("Enter the number of sides of the dice : ")
@@ -76,15 +122,11 @@ if __name__== '__main__':
                     help='Number of sides of the dice')
 
     args = parser.parse_args()
-    print(args)
 
-    # Possibility to launch with arg number of dice and numbre of throw ?
     if args.interactive:
         diceInteractive()
     elif args.gui:
         DiceGUI()
     else:
         diceCLI(args.sides, args.throws )
-
-
 
